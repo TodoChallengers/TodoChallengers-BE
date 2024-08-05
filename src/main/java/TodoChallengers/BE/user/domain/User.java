7660adb93b11ef6b817b7c937626171ddfbda66d
rumos <gyunjin3@gmail.com>
2024-08-05 22:18:36 +0900

    #4/feat user domain 추가

---
+++ src/main/java/TodoChallengers/BE/user/domain/User.java
@@ -0,0 +1,24 @@
+package TodoChallengers.BE.user.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    @Column(length = 100, nullable = false, unique = true)
+    private Long kakaoId; //로그인 식별키
+
+    private String nickname;
+
+    private String profileImage;
+}
