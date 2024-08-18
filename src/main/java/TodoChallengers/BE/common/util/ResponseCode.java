eb50f9d26fbbfdbb6016a5cb6a6a17bd8c477261
rumos <gyunjin3@gmail.com>
2024-08-19 00:52:25 +0900

    #4/feat 로그인 response code 200 추가

--- src/main/java/TodoChallengers/BE/common/util/ResponseCode.java
+++ src/main/java/TodoChallengers/BE/common/util/ResponseCode.java
@@ -31,7 +31,10 @@ public enum ResponseCode {
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, false, "서버에 오류가 발생하였습니다."),
 
     // 200 OK
-    USER_LOGINED(HttpStatus.OK, true, "로그인 되었습니다."),
+    USER_LOGIN_SUCCESS(HttpStatus.OK, true, "사용자 로그인 성공"),
+    USER_TOKEN_REISSUE_SUCCESS(HttpStatus.OK, true, "사용자 토큰 재발급 성공"),
+    USER_TOKEN_VALIDATE_SUCCESS(HttpStatus.OK, true, "사용자 토큰 검증 성공"),
+
 
     // 201 Created
     USER_CREATE_SUCCESS(HttpStatus.CREATED, true, "사용자 생성 성공");
