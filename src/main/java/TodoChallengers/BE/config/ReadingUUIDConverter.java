aadb20a2b2f6e11a643279330c1a1d91826cf2e2
hvunrnin <hmkwon0919@cau.ac.kr>
2024-08-23 13:48:48 +0900

    #10-3/feat converter 수정

--- src/main/java/TodoChallengers/BE/config/ReadingUUIDConverter.java
+++ src/main/java/TodoChallengers/BE/config/ReadingUUIDConverter.java
@@ -4,13 +4,17 @@ import org.bson.types.Binary;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.convert.ReadingConverter;
 
+import java.nio.ByteBuffer;
 import java.util.UUID;
 
 @ReadingConverter
 public class ReadingUUIDConverter implements Converter<Binary, UUID> {
     @Override
     public UUID convert(Binary source) {
-        return UUID.nameUUIDFromBytes(source.getData());
+        ByteBuffer byteBuffer = ByteBuffer.wrap(source.getData());
+        long mostSigBits = byteBuffer.getLong();
+        long leastSigBits = byteBuffer.getLong();
+        return new UUID(mostSigBits, leastSigBits);
     }
 }
 
--- src/main/java/TodoChallengers/BE/config/WritingUUIDConverter.java
+++ src/main/java/TodoChallengers/BE/config/WritingUUIDConverter.java
@@ -4,12 +4,16 @@ import org.bson.types.Binary;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.data.convert.WritingConverter;
 
+import java.nio.ByteBuffer;
 import java.util.UUID;
 
 @WritingConverter
 public class WritingUUIDConverter implements Converter<UUID, Binary> {
     @Override
     public Binary convert(UUID source) {
-        return new Binary(source.toString().getBytes());
+        ByteBuffer byteBuffer = ByteBuffer.allocate(16);
+        byteBuffer.putLong(source.getMostSignificantBits());
+        byteBuffer.putLong(source.getLeastSignificantBits());
+        return new Binary(byteBuffer.array());
     }
 }
